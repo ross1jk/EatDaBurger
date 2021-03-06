cf4d72f99c62643da4dc9070c4ec8c28e862c85f
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-03-05 20:26:02 -0500

    burger.js file set up

---
+++ models/burger.js
@@ -0,0 +1,22 @@
+const orm = require('../config/orm.js');
+
+const burger = {
+//   all(cb) {
+//     orm.all('cats', (res) => cb(res));
+//   },
+//   create(cols, vals, cb) {
+//     orm.create('cats', cols, vals, (res) => cb(res));
+//   },
+//   update(objColVals, condition, cb) {
+//     orm.update('cats', objColVals, condition, (res) => cb(res));
+//   },
+//   delete(condition, cb) {
+//     orm.delete('cats', condition, (res) => cb(res));
+//   },
+
+// selectAll()
+// insertOne()
+// updateOne()
+};
+
+module.exports = burger;
