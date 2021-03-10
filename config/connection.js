de302c1262e9ac9071c163b5b6cd8a5d086bb72c
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-03-09 21:20:45 -0500

    connection updated

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 const mysql = require('mysql');
+const connection;
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: process.env.dbPassword,
-  database: 'burgers_db',
-});
-
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: process.env.dbPassword,
+    database: 'burgers_db',
+  });
+};
 connection.connect((err) => {
   if (err) {
     console.error(`error connecting: ${err.stack}`);
